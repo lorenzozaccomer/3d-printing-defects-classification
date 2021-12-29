1637bd1330143e4cf97b024beca2d994ea11a53a
Lorenzo Zaccomer <lz.131034@gmail.com>
2021-12-29 21:29:32 +0100

    Upload with integration of image dataset folder and dataset size function

--- Project/project.py
+++ Project/project.py
@@ -1,14 +1,24 @@
 
 # Libraries
 import cv2
+import torch, torchvision, numpy, matplotlib
+
+from torchvision import datasets
+
+# Standard Library
 import os, sys
 
 # importing Functions from files
 from libs.dirs import *
+from libs.datasets import *
  
 # extract the train valid folders
-path = 'D:\\repositories\\3d-printer-recognition\\Images'
-folders = ReturnImageFolderFromPath(path)
+Path = 'D:\\repositories\\3d-printer-recognition\\Images'
+Subpaths = ['train','valid']
 
+image_datasets = ImagesDatasetFromFolders(Path, Subpaths)
+print(image_datasets)
 
+dataset_sizes = ImagesDatasetSize(image_datasets, Subpaths)
+print(dataset_sizes)
 
