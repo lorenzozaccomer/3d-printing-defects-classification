2784d088144b0254b67ed09c3aa40c3ff447b6c0
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-06 18:11:22 +0200

    ADD: image data_transform class, shuffle datasets and imshow function

--- Project/libs/datasets.py
+++ Project/libs/datasets.py
@@ -1,19 +1,54 @@
 
-import torch, torchvision
-import os
+import os, torch 
+import matplotlib.pyplot as plt
+import numpy as np
 
-from torchvision import datasets
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'valid': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+# FUNCTIONS
+
+def ShuffleDatasets(dataset, subs):
+    return {x: DataLoader(dataset[x], batch_size=4, num_workers=4, shuffle=True) for x in subs}
+
 
 def ImagesDatasetFromFolders(path, subs):
     '''
     Will create 2 dataset, one from train folder
     and one from valid folder
     '''
-    return {x: datasets.ImageFolder(os.path.join(path, x), x)
-                  for x in subs}
+    return {x: datasets.ImageFolder(os.path.join(path, x), data_transforms[x]) for x in subs}
 
 def ImagesDatasetSize(dataset, subs):
     '''
     Return the length of the image datasets
     '''
     return {x: len(dataset[x]) for x in subs}
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.figure(figsize=(15, 12))
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)
--- Project/project.py
+++ Project/project.py
@@ -1,7 +1,7 @@
 
 # Libraries
 import cv2
-import torch, torchvision, numpy, matplotlib
+import torch, torchvision
 
 from torchvision import datasets
 
@@ -11,13 +11,29 @@ import os, sys
 # importing Functions from files
 from libs.dirs import *
 from libs.datasets import *
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
  
-# extract the train valid folders
-Path = 'D:\\repositories\\3d-printer-recognition\\Images'
+# extract the train-valid images folders
+Path = 'U:\\repositories\\3d-printer-recognition\\Images'
 Subpaths = ['train','valid']
 
 image_datasets = ImagesDatasetFromFolders(Path, Subpaths)
-print(image_datasets)
+#print(image_datasets)
+
+dataloaders = ShuffleDatasets(image_datasets, Subpaths)
+print(dataloaders)
 
 dataset_sizes = ImagesDatasetSize(image_datasets, Subpaths)
 print(dataset_sizes)
+
+# Extract the class from one dataset (are equal between them)
+class_names = image_datasets['train'].classes
+print(class_names)
+
+# Get 1 batch images
+inputs, classes = next(iter(dataloaders['train']))
+
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
