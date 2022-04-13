7050f5bd4b4fdf6c80f69d0c0fb52ddc11130fc0
Lorenzo on VM <lz.131034@gmail.com>
2022-04-13 18:29:15 +0200

    reduced only for testing

--- Project/project.py
+++ Project/project.py
@@ -35,7 +35,7 @@ print(dataset_sizes)
 class_names = image_datasets['train'].classes
 #print(class_names)
 
-generate_batch_images(dataloaders,class_names)
+#generate_batch_images(dataloaders,class_names)
 
 model_ft = models.resnet18(pretrained=True)
 num_ftrs = model_ft.fc.in_features
@@ -53,7 +53,7 @@ optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-generated_model = train_model(dataloaders, dataset_sizes, Subpaths, model_ft, criterion, optimizer_ft, exp_lr_scheduler)
+generated_model = train_model(dataloaders, dataset_sizes, Subpaths, model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=2)
 
 visualize_model(dataloaders, class_names, generated_model)
 
