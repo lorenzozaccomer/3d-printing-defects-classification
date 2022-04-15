1af3ed65cbee74c604b58677fc030a80a6950829
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-15 10:51:51 +0200

    not important test update

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ path = r'U:\repositories\3d-printer-recognition\Project\test\geeks14.png'
 #path = r'D:\NoDefects-0_jpg.rf.8df76f71bc232b121de110483cc6e24b.jpg'
   
 # Using cv2.imread() method
-img = cv2.imread(path)
+img = cv2.imread('image28.jpg')
   
 # Displaying the image
 cv2.imshow('image', img)
