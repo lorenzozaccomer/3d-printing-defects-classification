400c9ca28ed84ced4e1cff39c61305ea294e15f7
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-16 19:23:51 +0200

    upload of new test file

---
+++ Project/test/test.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Transfer Learning for Computer Vision Tutorial
+==============================================
+**Author**: `Sasank Chilamkurthy <https://chsasank.github.io>`_
+In this tutorial, you will learn how to train a convolutional neural network for
+image classification using transfer learning. You can read more about the transfer
+learning at `cs231n notes <https://cs231n.github.io/transfer-learning/>`__
+Quoting these notes,
+    In practice, very few people train an entire Convolutional Network
+    from scratch (with random initialization), because it is relatively
+    rare to have a dataset of sufficient size. Instead, it is common to
+    pretrain a ConvNet on a very large dataset (e.g. ImageNet, which
+    contains 1.2 million images with 1000 categories), and then use the
+    ConvNet either as an initialization or a fixed feature extractor for
+    the task of interest.
+These two major transfer learning scenarios look as follows:
+-  **Finetuning the convnet**: Instead of random initialization, we
+   initialize the network with a pretrained network, like the one that is
+   trained on imagenet 1000 dataset. Rest of the training looks as
+   usual.
+-  **ConvNet as fixed feature extractor**: Here, we will freeze the weights
+   for all of the network except that of the final fully connected
+   layer. This last fully connected layer is replaced with a new one
+   with random weights and only this layer is trained.
+"""
+# License: BSD
+# Author: Sasank Chilamkurthy
+
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import os
+
+from test_training import *
+from test_visualizer import *
+
+cudnn.benchmark = True
+plt.ion()   # interactive mode
+
+######################################################################
+# Load Data
+# ---------
+#
+# We will use torchvision and torch.utils.data packages for loading the
+# data.
+#
+# The problem we're going to solve today is to train a model to classify
+# **ants** and **bees**. We have about 120 training images each for ants and bees.
+# There are 75 validation images for each class. Usually, this is a very
+# small dataset to generalize upon, if trained from scratch. Since we
+# are using transfer learning, we should be able to generalize reasonably
+# well.
+#
+# This dataset is a very small subset of imagenet.
+#
+# .. Note ::
+#    Download the data from
+#    `here <https://download.pytorch.org/tutorial/hymenoptera_data.zip>`_
+#    and extract it to the current directory.
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = 'L:\\test\\Images'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+######################################################################
+# Training the model
+# ------------------
+#
+# Now, let's write a general function to train a model. Here, we will
+# illustrate:
+#
+# -  Scheduling the learning rate
+# -  Saving the best model
+#
+# In the following, parameter ``scheduler`` is an LR scheduler object from
+# ``torch.optim.lr_scheduler``.
+
+
+######################################################################
+# Finetuning the convnet
+# ----------------------
+#
+# Load a pretrained model and reset final fully connected layer.
+#
+
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+# Here the size of each output sample is set to 2.
+# Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+model_ft.fc = nn.Linear(num_ftrs, 2)
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+######################################################################
+# Train and evaluate
+# ^^^^^^^^^^^^^^^^^^
+#
+# It should take around 15-25 min on CPU. On GPU though, it takes less than a
+# minute.
+#
+
+model_ft = train_model(dataloaders, dataset_sizes,model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=1)
+
+######################################################################
+#
+
+visualize_model(dataloaders, class_names, model_ft)
+
+######################################################################
+#
+
+plt.ioff()
+plt.show()
+
+######################################################################
+# Further Learning
+# -----------------
+#
+# If you would like to learn more about the applications of transfer learning,
+# checkout our `Quantized Transfer Learning for Computer Vision Tutorial <https://pytorch.org/tutorials/intermediate/quantized_transfer_learning_tutorial.html>`_.
+#
+#
---
+++ Project/test/test_visualizer.py
@@ -0,0 +1,41 @@
+
+from matplotlib import pyplot as plt
+import numpy as np
+import torch
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+def visualize_model(dataloaders, class_names, model, num_images=6):
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title(f'predicted: {class_names[preds[j]]}')
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    return
