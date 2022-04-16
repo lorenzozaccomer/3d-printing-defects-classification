c67edf00da45b36e3a2fa07d553eb8a8d39eecb5
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-16 19:25:40 +0200

    little changes on project.py

--- Project/project.py
+++ Project/project.py
@@ -1,12 +1,14 @@
 
 # Libraries
 import cv2
-import torch, torchvision
+import torch
 
 import torch.nn as nn
-import torch.optim  as optim
-
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
 from torchvision import models
+import matplotlib.pyplot as plt
 
 # importing Functions from files
 from libs.dirs import *
@@ -14,8 +16,12 @@ from libs.datasets import *
 from libs.training import *
 from libs.visualizer import *
 
-DesiredPath = "U:\\repositories\\3d-printer-recognition\\Images"
+cudnn.benchmark = True
+plt.ion()   # interactive mode
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+DesiredPath = "U:\\repositories\\3d-printer-recognition\\Images"
 
 Path, Subpaths = CheckCurrentPathAndExtractSubPaths(DesiredPath)
 
@@ -23,21 +29,22 @@ print(Path)
 print(Subpaths)
 
 image_datasets = ImagesDatasetFromFolders(Path, Subpaths)
-#print(image_datasets)
 
 dataloaders = ShuffleDatasets(image_datasets, Subpaths)
-#print(dataloaders)
 
 dataset_sizes = ImagesDatasetSize(image_datasets, Subpaths)
 print(dataset_sizes)
 
-# Extract the class from one dataset (are equal between them)
+# # Extract the class from one dataset (are equal between them)
 class_names = image_datasets['train'].classes
-#print(class_names)
 
-#generate_batch_images(dataloaders,class_names)
+#generate_batch_images(dataloaders,class_names, 'train')
 
 model_ft = models.resnet18(pretrained=True)
+
+for param in model_ft.parameters():
+    param.requires_grad = False
+
 num_ftrs = model_ft.fc.in_features
 # Here the size of each output sample is set to 2.
 # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
@@ -51,11 +58,17 @@ criterion = nn.CrossEntropyLoss()
 optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 
 # Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-generated_model = train_model(dataloaders, dataset_sizes, Subpaths, model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=2)
+generated_model = train_model(dataloaders, dataset_sizes, model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=1)
 
-visualize_model(dataloaders, class_names, generated_model, num_images=2)
+visualize_model(dataloaders, class_names, generated_model)
 
 plt.ioff()
 plt.show()
+
+def save_model(model, optimizer):
+    checkpoint = {'state_dict' : model.state_dict(), 'optimizer' : optimizer.state_dict()}
+    torch.save(checkpoint, './alexnet_extractor_model.pth')
+
+save_model(generated_model, optimizer_ft)
