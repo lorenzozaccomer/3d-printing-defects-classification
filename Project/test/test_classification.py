f4409887fcf990be004bd1194144e17dfe46c229
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-19 12:28:38 +0200

    not important changes on test_classification.py file

--- Project/test/test_classification.py
+++ Project/test/test_classification.py
@@ -6,14 +6,13 @@ import torch
 from torchvision import transforms, models
 
 
-transform = transforms.Compose([            #[1]
- transforms.Resize(256),                    #[2]
- transforms.CenterCrop(224),                #[3]
- transforms.ToTensor(),                     #[4]
- transforms.Normalize(                      #[5]
- mean=[0.485, 0.456, 0.406],                #[6]
- std=[0.229, 0.224, 0.225]                  #[7]
- )])
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+ ])
 
 dir(models)
 
