c52dc4285819a4f59d497b73626f8346ba4a2376
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-19 11:49:10 +0200

    test image classification files

---
+++ Project/test/test_classification.py
@@ -0,0 +1,42 @@
+
+
+from PIL import Image
+
+import torch
+from torchvision import transforms, models
+
+
+transform = transforms.Compose([            #[1]
+ transforms.Resize(256),                    #[2]
+ transforms.CenterCrop(224),                #[3]
+ transforms.ToTensor(),                     #[4]
+ transforms.Normalize(                      #[5]
+ mean=[0.485, 0.456, 0.406],                #[6]
+ std=[0.229, 0.224, 0.225]                  #[7]
+ )])
+
+dir(models)
+
+img = Image.open("L:\\Università\\repositories\\3d-printer-recognition\\Project\\test\\dog.jpg")
+
+img_t = transform(img)
+batch_t = torch.unsqueeze(img_t, 0)
+
+# Load alexnet model
+alexnet = models.alexnet(pretrained=True)
+
+# Put our model in eval mode
+alexnet.eval()
+
+# Carry out inference
+out = alexnet(batch_t)
+print(out.shape)
+
+# Load labels
+with open('L:\\Università\\repositories\\3d-printer-recognition\\Project\\test\\imagenet_classes.txt') as f:
+  classes = [line.strip() for line in f.readlines()]
+
+_, index = torch.max(out, 1)
+percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+print(classes[index[0]], percentage[index[0]].item())
+
