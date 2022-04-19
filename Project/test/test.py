c1e8d4679be5d24c8172bcae12d7768259378f86
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-19 12:02:51 +0200

    test_dataset generation + changes on test.py file

--- Project/test/test.py
+++ Project/test/test.py
@@ -40,6 +40,7 @@ import os
 
 from test_training import *
 from test_visualizer import *
+from test_datasets import *
 
 cudnn.benchmark = True
 plt.ion()   # interactive mode
@@ -67,29 +68,15 @@ plt.ion()   # interactive mode
 
 # Data augmentation and normalization for training
 # Just normalization for validation
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
 
 data_dir = 'L:\\test\\Images'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                             shuffle=True)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+
+image_datasets = TEST__ImagesDatasetFromFolders(data_dir)
+
+dataloaders = TEST__ShuffleDatasets(image_datasets)
+
+dataset_sizes = TEST__ImagesDatasetSize(image_datasets)
+
 class_names = image_datasets['train'].classes
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
