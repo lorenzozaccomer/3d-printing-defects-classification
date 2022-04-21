2d615a68c991bb4cf4a5286734f0cc72ea3c661a
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-21 23:22:15 +0200

    class.py: fixed PATH_MODEL

--- Project/test/classification.py
+++ Project/test/classification.py
@@ -19,8 +19,8 @@ transform = transforms.Compose([
     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
  ])
 
-PATH_MODEL = './generated_model.pth'
-PATH_IMAGE = "L:\\Università\\repositories\\3d-printer-recognition\\Project\\test\\5.jpg"
+PATH_MODEL = 'L:\\Università\\repositories\\3d-printer-recognition\\generated_model.pth'
+PATH_IMAGE = "L:\\Università\\repositories\\3d-printer-recognition\\Project\\test\\8.jpg"
 classes = ['NoDefects', 'YesDefects']
 
 # Load model
@@ -31,7 +31,6 @@ evaluation_model.eval()
 img = Image.open(PATH_IMAGE)
 image = transform(img)
 
-
 # Carry out inference
 tensor_image = image.unsqueeze(0)
 out = evaluation_model(tensor_image)
@@ -39,8 +38,6 @@ out = evaluation_model(tensor_image)
 _, indices = torch.max(out,1)
 
 percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
-
-print(classes[indices.item()], percentage[indices].item())
 
 imshow(image, "Prediction: " + classes[indices.item()] + " " + str(percentage[indices].item()))
 
