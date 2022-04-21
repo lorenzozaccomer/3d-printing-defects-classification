bd0bee9ba39e0d7234d78be146129f2e15d6c47c
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-21 23:04:35 +0200

    classification.py: show test image

--- Project/test/classification.py
+++ Project/test/classification.py
@@ -4,7 +4,12 @@ from PIL import Image
 from numpy import indices
 
 import torch
+import matplotlib.pyplot as plt
 from torchvision import transforms
+
+from test_visualizer import imshow
+
+plt.ion()   # interactive mode
 
 transform = transforms.Compose([
     transforms.Resize(256),
@@ -15,7 +20,7 @@ transform = transforms.Compose([
  ])
 
 PATH_MODEL = './model_ft.pth'
-PATH_IMAGE = """L:\\Università\\repositories\\3d-printer-recognition\\Project\\test\\5.jpg"""
+PATH_IMAGE = "L:\\Università\\repositories\\3d-printer-recognition\\Project\\test\\5.jpg"
 classes = ['NoDefects', 'YesDefects']
 
 # Load model
@@ -24,22 +29,23 @@ evaluation_model.eval()
 
 #Load image
 img = Image.open(PATH_IMAGE)
-#img.show()
 image = transform(img)
-image = image.unsqueeze(0)
+imshow(image.unsqueeze(0))
+
 
 # Carry out inference
-out = evaluation_model(image)
-print(out.shape)
+tensor_image = image.unsqueeze(0)
+out = evaluation_model(tensor_image)
+#print(out.shape)
 
 classes = ['NoDefects', 'YesDefects']
 
 _, indices = torch.max(out,1)
 
-print(indices.item())
-
 percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
 
-print(percentage)
-
 print(classes[indices.item()], percentage[indices].item())
+
+
+plt.ioff()
+plt.show()
