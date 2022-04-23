6f4aa87338c81e84df9bfdd4c2b14cc6524aa8f2
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-23 12:44:12 +0200

    update of lr parameter (to 0.001 to 0.05) + update associate generated_model

--- Project/project.py
+++ Project/project.py
@@ -69,7 +69,7 @@ if iteration == 0: # I want to generate a model
     criterion = nn.CrossEntropyLoss()
 
     # Observe that all parameters are being optimized
-    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.05, momentum=0.9)
 
     # Decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
