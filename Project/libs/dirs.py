e7ecc8e8dca106120af26ccf20b8c45d32fae9ce
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-04-23 16:33:00 +0200

    remove not used function from dirs.py file

--- Project/libs/dirs.py
+++ Project/libs/dirs.py
@@ -8,24 +8,10 @@
 import os
 
 
-def ReturnImageFolderFromPath(path):
-    """
-    Return the folders on Image folder
-    """
-    return os.listdir(path)
-
-
-def FoldersOnImageList(path):
-    """
-    Return the paths from primary folder
-    """
-    return list((os.path.join(path,folder) for folder in ['train', 'valid']))
-
-
 def CheckCurrentPathAndExtractSubPaths(DesiredPath):
     """
     Check if the path that you have previously set is right,
-    also return the dirs on the path,
+    also return the directories on the path,
     otherwelse it will terminate the script,
     """
 
